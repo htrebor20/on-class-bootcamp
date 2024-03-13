38570637b41f1e9566db782a69f0ea454dea4bfb
Robert <htrebor20@gmail.com>
2024-03-12 23:10:45 -0500

    first commit

---
+++ src/main/java/com/pragma/onclass/OnClassBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.onclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnClassBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnClassBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/onclass/OnClassBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.onclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnClassBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
