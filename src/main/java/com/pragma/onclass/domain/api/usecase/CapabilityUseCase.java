b279a80c9f2c8fd82522cf276fdd6275fa5d7bf1
Robert <htrebor20@gmail.com>
2024-03-20 22:21:11 -0500

    feat: add capacity validation

--- src/main/java/com/pragma/onclass/domain/api/usecase/CapabilityUseCase.java
+++ src/main/java/com/pragma/onclass/domain/api/usecase/CapabilityUseCase.java
@@ -1,6 +1,7 @@
 package com.pragma.onclass.domain.api.usecase;
 
 import com.pragma.onclass.domain.api.ICapabilityServicePort;
+import com.pragma.onclass.domain.exception.TechnologyAlreadyExistsException;
 import com.pragma.onclass.domain.model.Capability;
 import com.pragma.onclass.domain.spi.ICapabilityPersistencePort;
 
@@ -13,6 +14,9 @@ public class CapabilityUseCase implements ICapabilityServicePort {
 
     @Override
     public void saveCapability(Capability capability) {
+        if(capability.getTechnologies().size()<=3 & capability.getTechnologies().size()<=20) {
+            throw new TechnologyAlreadyExistsException();
+        }
         capabilityPersistencePort.saveCapability(capability);
     }
 }
